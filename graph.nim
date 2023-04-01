ffdd8bf5727ae18089d0db219e37286ee982eeca
Morgan Holly <98441016+morganholly@users.noreply.github.com>
2023-04-01 00:30:36 -0400

    mutable returns from procs that return first arg

--- graph.nim
+++ graph.nim
@@ -3,12 +3,12 @@ import std/[math]
 
 const e_math = 2.7182818284590452353602874713526624977572470936999595749669676277
 
-proc add* (graph: var Graph, node: GraphNode): Graph =
+proc add* (graph: var Graph, node: GraphNode): var Graph =
     graph.nodes.add(node)
     result = graph
 
-proc connect* (graph: var Graph, n1, n2: var GraphNode, p1, p2: int, component: ComponentVariant, spring: float = 1): Graph =
-    var spring = SpringState(rest_length: spring, current_length: spring, force: 0.0, stiffness: 2)
+proc connect* (graph: var Graph, n1, n2: var GraphNode, p1, p2: int, component: ComponentVariant, spring: float = 1): var Graph =
+    var spring = SpringState(rest_length: spring, current_length: spring, force: 0.0, stiffness: 2.0)
     var gc1 = GraphConnection(component: component, from_pin: p1, to_pin: p2, spring: spring)
     var gc2 = GraphConnection(component: component, from_pin: p2, to_pin: p1, spring: spring)
     n1.connections.add(gc1)
@@ -19,7 +19,7 @@ proc connect* (graph: var Graph, n1, n2: var GraphNode, p1, p2: int, component: 
 proc lerp (x, y, mix: float): float =
     result = mix * y + (1 - mix) * x
 
-proc spring_force* (spring: var SpringState, skew: float): SpringState =
+proc spring_force* (spring: var SpringState, skew: float = 2.0): var SpringState =
     let delta = spring.current_length - spring.rest_length
     let clamped = min(max((delta * 0.5) + 0.75, 0), 1)
     let clamped_nl = 2 * clamped * clamped
